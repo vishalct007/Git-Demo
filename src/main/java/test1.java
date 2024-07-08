be3637304e9daf035d6e97fab5b878bb8f7ec54c
ctvishal007 <ctvishal007@gmail.com>
2024-07-08 14:54:18 +0530

    first commit

---
+++ src/main/java/test1.java
@@ -0,0 +1,14 @@
+
+public class test1 {
+	public static void main(String[] args) {
+		String s="vishal";
+		
+		for(int i=s.length()-1 ; i>=0 ; i--) {
+			  System.out.print(s.charAt(i));
+		}
+
+	}
+
+
+
+}
